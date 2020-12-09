5f9747249150e03aef9507a80a0eb76e6fd71a10
David Ramos del Rio <ramos.del.rio.david@alumnat.copernic.cat>
2020-12-09 11:39:31 +0100

    utilizamos pront

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ window.onload = function (){
     mostrarJuego.style.display ="none";
     estadisticas.style.display ="none";
 
+    var NumFila = prompt("Introduce el numero de filas ").split(" , ");   
+    var NumColumna = prompt("Introduce el numero de columnas ").split(" , ");
     
+    document.write("El numero de filas que has introducido es; " + NumFila + " El numero de columnas que has introducido es; " + NumColumna);
 }
