21b73ea261402a29fa1cd2656cdc676fad22ca7d
David Ramos del Rio <ramos.del.rio.david@alumnat.copernic.cat>
2020-12-14 21:16:30 +0100

    partida js actualizado

---
+++ partida.js
@@ -0,0 +1,19 @@
+let partida = {
+    medida_tabla = 5,
+    medidaida_casillas_tabla = 25,
+    tabla:[],
+	/*Funciones*/
+	
+    inicializar_tabla:function()´{
+		for(let i = 0 ; i < medida_tabla; i ++)
+			tabla [i] = [];	
+		for(let j = 0; j < array.length;j++){
+			const element = array[j]
+		}			
+	}
+	
+    inciar:function (mida){
+        this.mida_tauler= mida;
+        this.mida_caselles_tauler = mida * mida;
+    }
+}
