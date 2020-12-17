e2721f827ae3ba46873657985c40d455824e048d
VictorCoboCopernic <cobo.jandula.victor@alumnat.copernic.cat>
2020-12-17 12:22:35 +0100

    timeout popup

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 window.onload = function(){
     /*Oculta los divs*/
     estadisticas.style.display ="none";
-    PreguntarMedida();  
+    setTimeout(PreguntarMedida, 250);
 }
 
-window.onload = function PreguntarMedida(){ 
+function PreguntarMedida(){ 
     let medida = prompt("Introduce la altura del tablero (entre 5 y 20)");
     InicioJuego(medida);
 }
 
-
 function InicioJuego(medida){
-if (medida>=5 && medida<=20 && medida % 1 == 0){
-    alert("La tabla creada es de " + medida + "x" + medida);
-    partida.mostrarTabla(medida);
-}else{
-    alert("El valor introducido es incorrecto");
-    PreguntarMedida();  
+    if (medida>=5 && medida<=20 && medida % 1 == 0){
+        alert("La tabla creada es de " + medida + "x" + medida);
+        partida.mostrarTabla(medida);
+    }else{
+        alert("El valor introducido es incorrecto, comprueba que es un número entre 5 y 20");
+        PreguntarMedida();  
+    }
 }
-
-
-
-
-        }
