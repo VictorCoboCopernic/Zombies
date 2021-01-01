e4880e2ad2c470f3abebc5c46ba5a349ec71318d
VictorCoboCopernic <cobo.jandula.victor@alumnat.copernic.cat>
2021-01-01 16:29:11 +0100

    rellenar rehecho

--- partida.js
+++ partida.js
@@ -13,7 +13,6 @@ let partida = {
             for (let DibFilas = 0; DibFilas < medida; DibFilas++){
                 dibujarTabla += "<td> g </td>";
                 IdentificadorCasilla+=1;
-                console.log(IdentificadorCasilla);
             }
             dibujarTabla += "</tr>";
         }
@@ -25,63 +24,145 @@ let partida = {
 
 
     inicializaMatriz:function(medida){
+        partida.creaTabla(medida);
+        partida.rellenaRecompensa(medida);
         partida.rellenaEstrella(medida);
         partida.rellenaZombie(medida);
+        partida.rellenaGrass(medida);
         console.log(this.tabla)
     },
     
-    rellenaEstrella:function(medida){
-        let contadorEstrellas = 0 ;
-        let IdentificadorCasilla = 1 ;
+    creaTabla:function(medida){
         for(let i =0;i < medida; i++){
             this.tabla[i] = [];
             for(let j = 0; j < medida;j++ ){
-                let estrellaAleatori = Math.floor(Math.random() * (medida/4*3));  
-                if (contadorEstrellas == medida){
-                    this.tabla[i][j] = " ";
-                }else if(((medida*medida)-IdentificadorCasilla)==(medida-contadorEstrellas)){
-                    this.tabla[i][j] = "e";
-                    contadorEstrellas+=1;
-                }else if (estrellaAleatori==0){
-                    this.tabla[i][j] = "e";
-                    contadorEstrellas+=1;
-                }else{
-                    this.tabla[i][j] = " ";
-                }
-                IdentificadorCasilla+=1;
+                this.tabla[i][j] = " ";
             }
         }
-        console.log("Estrellas: " + contadorEstrellas)
+    },
+    
+    rellenaRecompensa:function(medida){
+        let totalRecompensa = Math.floor((medida*medida)/4) ;
+        let contadorRecompensa = 0 ;
+        let medidaRecompensa = 3 ;
+        let BarcosObligatorios = 0 ;
+        while(contadorRecompensa<totalRecompensa){
+            let randomMedidaRecompensa = Math.floor(Math.random() * 3) + 1;
+            if(BarcosObligatorios==1){
+                let medidaRecompensa = randomMedidaRecompensa ;
+            }
+            let randomX = Math.floor(Math.random() * medida);
+            let randomY = Math.floor(Math.random() * medida);
+            let Orien = Math.floor(Math.random() * 4);
+            if((contadorRecompensa + medidaRecompensa) <=  totalRecompensa){
+                if(this.tabla[randomY][randomX] = " "){
+                    let correcte = 0 ;
+                    if(Orien==0){
+                        for(let i =0;i < medidaRecompensa; i++){
+                            if(this.tabla[randomY][randomX+i] = " "){
+                                if(correcte==medidaRecompensa){
+                                    this.tabla[randomY][randomX+i] = ("r" + medidaRecompensa)
+                                    contadorRecompensa+=1
+                                    medidaRecompensa-=1
+                                    if(medidaRecompensa==0){
+                                        let BarcosObligatorios = 1 ;
+                                    }
+                                }else{
+                                correcte+=1
+                                }
+                            }
+                        }
+                    }
+                    if(Orien==1){
+                        for(let i =0;i < medidaRecompensa; i++){
+                            if(this.tabla[randomY][randomX-i] = " "){
+                                if(correcte==medidaRecompensa){
+                                    this.tabla[randomY][randomX-i] = ("r" + medidaRecompensa)
+                                    contadorRecompensa+=1
+                                    medidaRecompensa-=1
+                                    if(medidaRecompensa==0){
+                                        let BarcosObligatorios = 1 ;
+                                    }
+                                }else{
+                                correcte+=1
+                                }
+                            }
+                        }
+                    }
+                    if(Orien==2){
+                        for(let i =0;i < medidaRecompensa; i++){
+                            if(this.tabla[randomY+i][randomX] = " "){
+                                if(correcte==medidaRecompensa){
+                                    this.tabla[randomY+i][randomX] = ("r" + medidaRecompensa)
+                                    contadorRecompensa+=1
+                                    medidaRecompensa-=1
+                                    if(medidaRecompensa==0){
+                                        let BarcosObligatorios = 1 ;
+                                    }
+                                }else{
+                                correcte+=1
+                                }
+                            }
+                        }
+                    }
+                    if(Orien==3){
+                        for(let i =0;i < medidaRecompensa; i++){
+                            if(this.tabla[randomY-i][randomX] = " "){
+                                if(correcte==medidaRecompensa){
+                                    this.tabla[randomY-i][randomX] = ("r" + medidaRecompensa)
+                                    contadorRecompensa+=1
+                                    medidaRecompensa-=1
+                                    if(medidaRecompensa==0){
+                                        let BarcosObligatorios = 1 ;
+                                    }
+                                }else{
+                                correcte+=1
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    
+        rellenaEstrella:function(medida){
+        let contadorEstrellas = 0 ;
+        while(contadorEstrellas<medida){
+            let randomX = Math.floor(Math.random() * medida);
+            let randomY = Math.floor(Math.random() * medida);
+            if(this.tabla[randomY][randomX] = " "){
+                this.tabla[randomY][randomX] = "e";
+                contadorEstrellas+=1
+            }
+        }
+    },
+    
+    rellenaZombie:function(medida){
+        let contadorZombies = 0 ;
+        let totalZombies = Math.floor((medida*medida)/4);
+        while(contadorZombies<totalZombies){
+            let randomX = Math.floor(Math.random() * medida);
+            let randomY = Math.floor(Math.random() * medida);
+            if(this.tabla[randomY][randomX] = " "){
+                this.tabla[randomY][randomX] = "z";
+                contadorZombies+=1
+            }
+        }
     },
     
     
-    rellenaZombie:function(medida){
-        let contadorEstrellas = 0 ;
-        let contadorZombies = 0 ;
-        let IdentificadorCasilla = 1 ;
+
+    
+    
+    rellenaGrass:function(medida){
         for(let i =0;i < medida; i++){
             for(let j = 0; j < medida;j++ ){
-                //let zombieAleatori = Math.floor(Math.random() * ((medida*medida)/4));
-                if(this.tabla[i][j] == "e"){
-                    contadorEstrellas+=1;
-                }else if (contadorZombies == (Math.floor((medida*medida)/4))){
-                    this.tabla[i][j] = " ";
-                }else if(((medida*medida)-IdentificadorCasilla)<=((Math.floor((medida*medida)/4))-contadorZombies)-(medida-contadorEstrellas)){
-                    this.tabla[i][j] = "z";
-                    contadorZombies+=1;
-                }else{
-                    let zombieAleatori = Math.floor(Math.random() * (((medida*medida/4*3)-medida)/(medida*medida/4)));
-                    if (zombieAleatori==0){
-                        this.tabla[i][j] = "z";
-                        contadorZombies+=1;
-                    }else{
-                        this.tabla[i][j] = " ";
-                    }
+                if(this.tabla[i][j] == " "){
+                    this.tabla[i][j] = "g";
                 }
-                IdentificadorCasilla+=1;
             }
         }
-        console.log("Zombies: " + contadorZombies)
     },
     
     
